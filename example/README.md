7f4013159a717ef3bb5639a6885f553efc9b0dda
aha <nagy.attis.81@gmail.com>
2022-08-04 14:33:06 +0200

    example

---
+++ example/README.md
@@ -0,0 +1,31 @@
+# pouet-query
+
+Example program for using pouet-dump npm package.
+
+## NPM package
+
+https://www.npmjs.com/package/@nagyattis81/pouet-dump
+
+```
+npm i @nagyattis81/pouet-dump
+```
+
+## Installing
+
+```
+npm i
+```
+
+## Sripts
+
+- for query
+
+```
+npm run query
+```
+
+- for dev
+
+```
+npm run test
+```
---
+++ example/src/query.ts
@@ -0,0 +1,33 @@
+import Pouet, { Prod } from '@nagyattis81/pouet-dump';
+
+Pouet.getLatest().subscribe((dumps) => {
+  const func = dumps.parties.data.find(
+    (party) => party.name.toLowerCase() === 'function',
+  )?.id;
+  if (!func) {
+    return;
+  }
+
+  const rows: any[] = [];
+  dumps.prods.data
+    .filter((prod) => prod.party?.id === func)
+    .sort((a: Prod, b: Prod) => {
+      if (a.voteup > b.voteup) {
+        return -1;
+      }
+      if (a.voteup < b.voteup) {
+        return 1;
+      }
+      return 0;
+    })
+    .filter((_, index) => index < 10)
+    .forEach((prod) => {
+      rows.push({
+        pouetid: prod.id,
+        'name type': prod.name + ' ' + prod.type,
+        voteup: prod.voteup,
+      });
+    });
+  console.log(dumps.prods.dump_date);
+  console.table(rows);
+});
