38f318d242110ff295fcd1d14a05fefbbbc9b6da
aha <nagy.attis.81@gmail.com>
2022-08-11 18:49:54 +0200

    example

--- query/src/main.ts
+++ query/src/main.ts
@@ -1,1 +1,16 @@
-console.log('E');
+import Pouet from '@nagyattis81/pouet-dump';
+
+Pouet.sqlQuery(
+  `
+  SELECT 'https://www.pouet.net/prod.php?which=' || P.id, P.name, P.voteup, P.party_year
+  FROM prod as P
+  INNER JOIN credits as C ON C.prod = P.id
+  INNER JOIN user as U ON U.id   = C.user
+  WHERE U.nickname LIKE 'aha'
+  ORDER BY P.voteup DESC
+  LIMIT 10
+  ;
+  `,
+).subscribe((result) => {
+  console.table(result);
+});
