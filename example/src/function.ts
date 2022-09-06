9cfd13de78011e26d3aa14a7b90e226cb100f786
aha <nagy.attis.81@gmail.com>
2022-09-06 10:07:17 +0200

    fix download

--- example/src/function.ts
+++ example/src/function.ts
@@ -1,7 +1,55 @@
-import Pouet from '@nagyattis81/pouet-dump';
+import Pouet, { Prod } from '@nagyattis81/pouet-dump';
 import axios from 'axios';
 import * as fs from 'fs';
-const ProgressBar = require('progress');
+import * as ProgressBar from 'progress';
+
+enum DownloadType {
+  DOWNLOAD,
+  NO_DOWNLOAD,
+}
+
+function download(prods: Prod[], downloadType: DownloadType) {
+  prods.forEach(async (prod) => {
+    let url = prod.download;
+    const PREFIX = 'https://files.scene.org/view/parties';
+    if (url.startsWith(PREFIX)) {
+      url =
+        'https://files.scene.org/get/parties' + url.substring(PREFIX.length);
+    }
+    const fields = url.split('/');
+    const prefix = String(prod.voteup).padStart(3, '0');
+    const file = prefix + '_' + fields[fields.length - 1];
+    if (downloadType === DownloadType.NO_DOWNLOAD) {
+      console.log(file);
+      return;
+    }
+
+    axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+    }).then((response) => {
+      const totalLength = response.headers['content-length'];
+      if (totalLength) {
+        const progressBar = new ProgressBar(
+          'downloading [:bar] :percent :etas ' + url,
+          {
+            width: 40,
+            complete: '=',
+            incomplete: ' ',
+            renderThrottle: 1,
+            total: parseInt(totalLength),
+          },
+        );
+
+        response.data.on('data', (chunk: any) => {
+          progressBar.tick(chunk.length);
+        });
+      }
+      response.data.pipe(fs.createWriteStream('./out/' + file));
+    });
+  });
+}
 
 Pouet.getLatest().subscribe((dumps) => {
   const FROM_DATE = 2017;
@@ -9,7 +57,7 @@ Pouet.getLatest().subscribe((dumps) => {
 
   const availTypes = ['4k', '8k', '16k', '32k', '64k', 'demo'];
   const expectTypes = ['procedural graphics'];
-  const result = dumps.prods.data
+  const prods = dumps.prods.data
     .filter(
       (filter) =>
         filter.party_year >= FROM_DATE && filter.party_year <= TO_DATE,
@@ -31,8 +79,6 @@ Pouet.getLatest().subscribe((dumps) => {
       }
       return true;
     })
-    .filter((filter) => filter.party_place < 4)
-    .filter((filter) => filter.voteup > 20)
     .sort((a, b) => {
       if (a.voteup < b.voteup) {
         return 1;
@@ -41,59 +87,7 @@ Pouet.getLatest().subscribe((dumps) => {
         return -1;
       }
       return 0;
-    });
-
-  console.log(result.length);
-  result.forEach(async (prod, index) => {
-    // if (index < 10)
-    {
-      /* console.log(
-        prod.name,
-        prod.voteup,
-        prod.voteavg,
-        prod.type,
-        prod.party.name,
-        prod.party_year,
-        prod.party_place,
-        prod.download,
-      ); */
-      let url = prod.download;
-      const PREFIX = 'https://files.scene.org/view/parties';
-      if (url.startsWith(PREFIX)) {
-        url =
-          'https://files.scene.org/get/parties' + url.substring(PREFIX.length);
-      }
-      const fields = url.split('/');
-      const file = fields[fields.length - 1];
-      console.log(url, file);
-      const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream',
-      });
-      {
-        const totalLength = response.headers['content-length'];
-        if (totalLength) {
-          const progressBar = new ProgressBar(
-            'downloading [:bar] :percent :etas ' + url,
-            {
-              width: 40,
-              complete: '=',
-              incomplete: ' ',
-              renderThrottle: 1,
-              total: parseInt(totalLength),
-            },
-          );
-
-          response.data.on('data', (chunk: any) => {
-            progressBar.tick(chunk.length);
-          });
-        }
-        const prefix = String(prod.voteup).padStart(3, '0');
-        response.data.pipe(
-          fs.createWriteStream('./out/' + prefix + '_' + file),
-        );
-      }
-    }
-  });
+    })
+    .slice(0, 40);
+  download(prods, DownloadType.DOWNLOAD);
 });
